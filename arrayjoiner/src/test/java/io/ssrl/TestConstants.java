c414583f912a735d664088e76e4f589367f0c486
Marco Mero <94mero@gmail.com>
2023-04-29 19:03:10 +0200

    test number arrayJoiner

--- arrayjoiner/src/test/java/io/ssrl/TestConstants.java
+++ arrayjoiner/src/test/java/io/ssrl/TestConstants.java
@@ -2,7 +2,7 @@ package io.ssrl;
 
 @SuppressWarnings("all")
 public abstract class TestConstants {
-    public static final int TESTS_TO_RUN = 100;
+    public static final int TESTS_TO_RUN = 20;
     public static final int MAX_LENGTH = 1000;
     public static final int MAX_VALUE = 1000;
 }
